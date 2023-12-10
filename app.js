54b5c3d699ab4dd610d74f522a96867594bb6d87
gautam-193723 <gautammahajan193723@gmail.com>
2023-12-10 16:01:17 +0530

    data was deleted

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// new feature is  add -from
-=======
-=======
->>>>>>> fdcfcf329cd534ebc64151f4c67c234faf32704b
 
 // new feature is  add -from
-
-// new feature is  add -button
-
-<<<<<<< HEAD
->>>>>>> fdcfcf329cd534ebc64151f4c67c234faf32704b
-=======
->>>>>>> fdcfcf329cd534ebc64151f4c67c234faf32704b
